a34635767aea1ca8f24e3988f5e7f2ac72250692
Luiz Paulo Gonçalves <37484860+LuizPauloLPG2@users.noreply.github.com>
2019-04-09 21:57:32 -0300

    Add files via upload

---
+++ config/Db.php
@@ -0,0 +1,21 @@
+<?php
+
+class Db{
+    
+    protected static $conexao = null;
+
+    public static function conexao()
+    {
+        try {
+
+            if(self::$conexao === null){
+                self::$conexao = new PDO("mysql:host=localhost;dbname=db_ibus;", "root", "",
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            }
+            return self::$conexao;
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+}
